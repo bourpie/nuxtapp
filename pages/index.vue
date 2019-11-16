0f04f8b1e79bbfc7fe194d2fc961a9ee0a1558fe
Pierre Bourgeois <pbcomweb@gmail.com>
2019-11-16 09:08:20 -0500

    Ajout des métadonnées

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="container">
-
+    <h1>{{title}}</h1>
   </div>
 </template>
 
 <script>
-
 export default {
-  components: {
-
+  data () {
+    return {
+      title: 'Bienvenu chez moi',
+      description: 'Un site Nuxt en construction'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: this.description }
+      ]
+    }
   }
 }
 </script>
