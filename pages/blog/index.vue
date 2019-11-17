56cbd869351e36dc443eaa49cdda5c9cfffffadc
Pierre Bourgeois <pbcomweb@gmail.com>
2019-11-17 06:06:43 -0500

    Connection à Netlify

---
+++ pages/blog/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="(blog, index) in blogPosts" :key="index">
+        <a :href="index">{{ blog.title }}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts () {
+      return this.$store.state.blogPosts
+    }
+  }
+}
+</script>
